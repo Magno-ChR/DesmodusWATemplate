faab88dde85c382fb0ace2c887959892ff993a32
Carlo Magno Chuyes Romero <carlo.chuyes@upds.edu.bo>
2023-07-09 09:19:23 -0400

    Añadido componenete login-logout y cambios en main layaout

--- DesmodusWATemplate/AuthStateProvider.cs
+++ DesmodusWATemplate/AuthStateProvider.cs
@@ -7,13 +7,21 @@ namespace DesmodusWATemplate
 {
     public class AuthStateProvider : AuthenticationStateProvider
     {
+        private readonly ILocalStorageService localStorage;
+
+        public AuthStateProvider(ILocalStorageService localStorage)
+        {
+            this.localStorage = localStorage;
+        }
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
 
-            string token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJJRCI6IjMiLCJEb2N1bWVudG8iOiI1Njg2Mjg1IiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IkNhcmxvIE1hZ25vIENodXllcyIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2dlbmRlciI6Ik1hc2N1bGlubyIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6InJvb3RAZXhhbXBsZS5jb20iLCJJZFJvbCI6IjIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJyb290IiwiZXhwIjoxNjg3OTkzODc5fQ.4pg1JjYUuRHUIsgiqvpQ6YWf167Qcr5UtmpFvYmPEec";
+            string token = await localStorage.GetItemAsStringAsync("token");
 
-            //var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
             var identity = new ClaimsIdentity();
+            if(!string.IsNullOrEmpty(token))
+                identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+
 
             var user =  new ClaimsPrincipal(identity);
             var state = new AuthenticationState(user);
